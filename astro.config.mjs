b42f26bbb143d307210fb1423234fd08506da6d2
Darius Zivertas <2293236+zzzarius@users.noreply.github.com>
2024-07-16 16:28:44 +0300

    Separate development env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+const config =
+  import.meta.env.MODE === "development"
+    ? defineConfig({
+        integrations: [mdx(), sitemap()],
+        output: "static",
+      })
+    : defineConfig({
+        site: "https://adaptdk.github.io",
+        base: "/speculationrules",
+        integrations: [mdx(), sitemap()],
+        output: "static",
+      });
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://adaptdk.github.io/speculationrules",
-  integrations: [mdx(), sitemap()],
-  output: "static",
-});
+export default config;
