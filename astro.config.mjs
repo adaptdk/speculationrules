ee4faf4b4c3a3c359b5117a5f33d7da749fe09fd
Darius Zivertas <2293236+zzzarius@users.noreply.github.com>
2024-07-17 12:10:16 +0300

    Use one config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,10 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
-const config =
-  import.meta.env.MODE === "development"
-    ? defineConfig({
-        site: "http://localhost:4321",
-        base: "/",
-        integrations: [mdx(), sitemap()],
-        output: "static",
-      })
-    : defineConfig({
-        site: "https://adaptdk.github.io",
-        base: "/speculationrules/",
-        integrations: [mdx(), sitemap()],
-        output: "static",
-      });
-
 // https://astro.build/config
-export default config;
+export default defineConfig({
+  site: "https://adaptdk.github.io",
+  base: "/speculationrules/",
+  integrations: [mdx(), sitemap()],
+  output: "static",
+});
